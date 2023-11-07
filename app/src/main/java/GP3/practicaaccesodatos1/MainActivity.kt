f37c0eacfd5deb5aaf55631100130a3480127129
Oscar gracia <113419592+OscarGrC@users.noreply.github.com>
2023-11-07 10:07:34 +0100

    commit inicial

---
+++ app/src/main/java/GP3/practicaaccesodatos1/MainActivity.kt
@@ -0,0 +1,11 @@
+package GP3.practicaaccesodatos1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
